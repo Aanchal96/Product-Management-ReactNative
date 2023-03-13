5b37df3f25607c649f09dd92dc2b11792c405cc0
Aanchal <bansalaanchal96@gmail.com>
2023-03-13 15:29:42 -0400

    App.js updated

--- App.js
+++ App.js
@@ -1,27 +1,12 @@
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
-// import ProductForm from "./components/ProductForm"; 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ProductForm from "./components/ProductForm"; 
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Hi, My name is Aanchal!</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route index path="/" exact element={<ProductForm />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <View style={styles.container}>
+      <Text>Hi, My name is Aanchal!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
